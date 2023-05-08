3022bc16e91948f029683186a37cb9e1e8514033
MarceloBotan <marcelobotandasilva@gmail.com>
2023-05-08 11:06:54 -0300

    modified:   printers/models.py - add model - Printer

--- printers/models.py
+++ printers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Printer(models.Model):
+    name = models.CharField(max_length=255, default='-', verbose_name='Nome')
+    branch = models.IntegerField(default=0, verbose_name='Filial')
+    serial_number = models.CharField(max_length=255, default='-', verbose_name='Nome')
+    date_update = models.DateField(default=timezone.now, blank=True)
+    branch_closed = models.BooleanField(default=False)
+    auth_attachment = models.FileField(blank=True, upload_to='vivobox_docs/%Y/%m', verbose_name='Autorização')
+
+    tracking_code = models.CharField(max_length=13, default='-', verbose_name='Código de Rastreio')
+
+    def __str__(self) -> str:
+        return self.serial_number
